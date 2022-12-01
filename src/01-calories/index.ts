7a677e3e42471502f7af0d94b5714d6b17a4001b
Fredrik Oterholt <fredrik@technologic.as>
2022-12-01 08:05:39 +0100

    Lint

--- src/01-calories/index.ts
+++ src/01-calories/index.ts
@@ -1,17 +1,16 @@
 import { readFileSync } from "fs";
 import { resolve } from "path";
 
-export const getAbsolutePath = relativePath =>
-    resolve(__dirname, relativePath);
+const getAbsolutePath = (relativePath) => resolve(__dirname, relativePath);
 
 const inputs = readFileSync(getAbsolutePath("inputs.txt"), "utf8");
 
-const data = inputs.split(/\n\n/)
-    .map(cal => cal.split(/\n/)
-        .map(n => parseInt(n, 10)))
-    .map(nums => nums.reduce((a, b) => a + b, 0))
-    .sort()
-    .reverse();
+const data = inputs
+  .split(/\n\n/)
+  .map((cal) => cal.split(/\n/).map((n) => parseInt(n, 10)))
+  .map((nums) => nums.reduce((a, b) => a + b, 0))
+  .sort()
+  .reverse();
 
-console.log(data[1]);
-console.log(data[1]+data[2]+data[3]);
+console.log(data[0]);
+console.log(data[0] + data[1] + data[2]);
