aecf061bf6985f713299d2424f993abd4fc89d2c
Fredrik Oterholt <fredrik@technologic.as>
2022-12-03 10:03:14 +0100

    day 3 part 2

--- src/03/index.ts
+++ src/03/index.ts
@@ -5,16 +5,33 @@ const getAbsolutePath = (relativePath) => resolve(__dirname, relativePath);
 
 const inputs = readFileSync(getAbsolutePath("inputs.txt"), "utf8");
 
-const data = inputs
-  .split(/\n/)
+const sacks = inputs.split(/\n/);
+
+const characterRank = (u: string) => {
+  const ascii = u.charCodeAt(0);
+  return [ascii - "a".charCodeAt(0) + 1, ascii - "A".charCodeAt(0) + 27].filter((n) => n > 0)[0];
+};
+
+const data = sacks
   .map((sack) => {
     const comp1 = sack.slice(0, sack.length / 2);
     const comp2 = sack.replaceAll(comp1, "");
     const i = [...comp1].filter((c) => comp2.includes(c));
-    const u = i[0] as string;
-    const ascii = u.charCodeAt(0);
-    return [ascii - "a".charCodeAt(0) + 1, ascii - "A".charCodeAt(0) + 27].filter((n) => n > 0)[0];
+    return characterRank(i[0] as string);
   })
   .reduce((a, b) => a + b);
 
+const data2 = Array.from(Array(sacks.length / 3).keys())
+  .map((g) => [sacks[g * 3 + 0], sacks[g * 3 + 1], sacks[g * 3 + 2]])
+  .map(
+    ([a, b, c]) =>
+      a
+        .split("")
+        .filter((item) => b.includes(item) && c.includes(item))
+        .sort((x, y) => x.length - y.length)[0][0]
+  )
+  .map(characterRank)
+  .reduce((a, b) => a + b);
+
 console.log("part 1", data);
+console.log("part 2", data2);
