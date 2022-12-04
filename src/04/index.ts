4ba43607831eae650e58c576a1f288675bb5b194
Fredrik Oterholt <fredrik@technologic.as>
2022-12-04 14:54:16 +0100

    day 4 cleanup

--- src/04/index.ts
+++ src/04/index.ts
@@ -10,7 +10,7 @@ const range = (n: { from: number; to: number }) => numberRange(n.to - n.from + 1
 
 const ranges = inputs
   .split(/\n/)
-  .map((l) => l.match("(\\d+)-(\\d+),(\\d+)-(\\d+)"))
+  .map((l) => l.match(/(\d+)-(\d+),(\d+)-(\d+)/))
   .map(([_, a1, a2, b1, b2]) => ({
     a: {
       from: parseInt(a1, 10),
@@ -20,24 +20,22 @@ const ranges = inputs
       from: parseInt(b1, 10),
       to: parseInt(b2, 10),
     },
+  }))
+  .map(({ a, b }) => ({
+    a: { ...a, range: range(a) },
+    b: { ...b, range: range(b) },
   }));
 
 const part1 = ranges.filter(({ a, b }) => {
-  const aRange = range(a);
-  const bRange = range(b);
-
-  const aInB = bRange.includes(a.from) && bRange.includes(a.to);
-  const bInA = aRange.includes(b.from) && aRange.includes(b.to);
+  const aInB = b.range.includes(a.from) && b.range.includes(a.to);
+  const bInA = a.range.includes(b.from) && a.range.includes(b.to);
 
   return bInA || aInB;
 }).length;
 
 const part2 = ranges.filter(({ a, b }) => {
-  const aRange = range(a);
-  const bRange = range(b);
-
-  const aInB = bRange.includes(a.from) || bRange.includes(a.to);
-  const bInA = aRange.includes(b.from) || aRange.includes(b.to);
+  const aInB = b.range.includes(a.from) || b.range.includes(a.to);
+  const bInA = a.range.includes(b.from) || a.range.includes(b.to);
 
   return bInA || aInB;
 }).length;
