5e689121c6f12a2decaec0800dcb8161a4d3df32
Fredrik Oterholt <fredrik@technologic.as>
2022-12-05 14:35:54 +0100

    day 5 part 1

---
+++ src/05/index.ts
@@ -0,0 +1,43 @@
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+const getAbsolutePath = (relativePath) => resolve(__dirname, relativePath);
+
+const inputs = readFileSync(getAbsolutePath("example.txt"), "utf8");
+
+const [instructions, moves] = inputs.split(/\n\n/);
+
+const initState = instructions.split(/\n/).map((row) =>
+  row
+    .split(/(\w{3}|.{4})/g)
+    .flat()
+    .filter((r) => r !== "")
+    .map((r) => r.replaceAll("[", "").replaceAll("]", ""))
+    .map((r) => r.trim())
+);
+const range = (size, add = 0) => [...new Array(size).keys()].map((i) => i + add);
+
+const cols = parseInt(initState.at(-1).at(-1), 10);
+const matrix = initState.slice(0, initState.length - 1).map((r) => range(cols).map((a) => r[a]));
+
+const rot = matrix[0].map((val, index) => matrix.map((row) => row[index]).reverse());
+const rotated = rot.map((row) => row.filter((i) => !!i));
+
+const part1 = moves
+  .split(/\n/)
+  .map((move) => {
+    const [, cnt, from, to] = move.match(/move (\d+) from (\d+) to (\d+)/);
+    return { cnt: Number(cnt), from: Number(from) - 1, to: Number(to) - 1 };
+  })
+  .reduce(
+    (prev, { cnt, from, to }) =>
+      range(cnt).reduce(([...p]) => {
+        p[to].push(p[from].pop());
+        return p;
+      }, prev),
+    rotated
+  )
+  .map((r) => r.at(-1))
+  .join("");
+
+console.log("part 1", part1);
