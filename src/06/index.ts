b6b0cd804dd0a25adbbb594556a64a476e3ea0df
Fredrik Oterholt <fredrik@technologic.as>
2022-12-06 09:53:37 +0100

    day 6 part 2

--- src/06/index.ts
+++ src/06/index.ts
@@ -5,17 +5,22 @@ const getAbsolutePath = (relativePath) => resolve(__dirname, relativePath);
 
 const inputs = readFileSync(getAbsolutePath("inputs.txt"), "utf8");
 
-const findSequence = (b: string) =>
-  b.split("").reduce(
-    (prev, c, i) => {
-      if (prev.i) return prev;
-      if (new Set(prev.seq.split("").slice(i - 4, i)).size === 4) return { i };
-      return { seq: prev.seq + c };
-    },
-    { seq: "", i: 0 }
-  ).i;
+const findSequence =
+  (seqSize = 4) =>
+  (b: string) =>
+    b.split("").reduce(
+      (prev, c, i) => {
+        if (prev.i) return prev;
+        if (new Set(prev.seq.split("").slice(i - seqSize, i)).size === seqSize) return { i };
+        return { seq: prev.seq + c };
+      },
+      { seq: "", i: 0 }
+    ).i;
 
 const lines = inputs.split(/\n/);
 
-const part1 = lines.map(findSequence);
+const part1 = lines.map(findSequence(4));
 console.log("part 1", part1);
+
+const part2 = lines.map(findSequence(14));
+console.log("part 2", part2);
