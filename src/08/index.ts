7edd28ec183c914a8881b9bc5616a1cb8d6702ef
Fredrik Oterholt <fredrik@technologic.as>
2022-12-08 22:21:03 +0100

    Types

--- src/08/index.ts
+++ src/08/index.ts
@@ -3,32 +3,41 @@ import rotateMatrix from "../helpers/rotateMatrix";
 
 const { input, fileName } = getInputs(__dirname);
 
-type TreeSurroundings = {
+const grid = input.split(/\n/).map((r) => r.split("").map(Number));
+const rotatedGrid = rotateMatrix(grid);
+
+type Surroundings = {
   neighbours: number[][];
   treeHeight: number;
   r: number;
   c: number;
 };
 
-const grid = input.split(/\n/).map((r) => r.split("").map(Number));
-const rotatedGrid = rotateMatrix(grid);
+type SurroundingsWithVisible = Surroundings & {
+  visible: boolean;
+};
 
-const surroundings: TreeSurroundings[] = grid.flatMap((row: number[], r) =>
+const surroundings: Surroundings[] = grid.flatMap((row: number[], r) =>
   row.map((treeHeight, c) => {
     const up = rotatedGrid[c].slice(row.length - r);
     const right = row.slice(c + 1);
-    const down = rotatedGrid[c].slice(0, row.length - r - 1);
-    const left = row.slice(0, c);
-    return { neighbours: [up, right, down, left], treeHeight, r, c };
+    const down = rotatedGrid[c].slice(0, row.length - r - 1).reverse();
+    const left = row.slice(0, c).reverse();
+    return { neighbours: [up, left, right, down], treeHeight, r, c };
   })
 );
 
-const isCoveredInAllDirections = ({ neighbours, treeHeight }: TreeSurroundings): boolean =>
-  neighbours.filter((nH) => Math.max(...nH) >= treeHeight).length === 4;
+const isVisible = ({ neighbours, treeHeight, ...surr }: Surroundings): SurroundingsWithVisible => ({
+  ...surr,
+  visible: neighbours.filter((nH) => Math.max(...nH) >= treeHeight).length === 4,
+  neighbours,
+  treeHeight,
+});
 
-const coveredTrees = surroundings.filter(isCoveredInAllDirections);
+const allTrees = surroundings.map(isVisible);
 
-const part1 = surroundings.length - coveredTrees.length;
+const part1 = allTrees.filter((i) => !i.visible).length;
+
 const part2 = "";
 
 console.group(`2022-day08 ${fileName}`);
