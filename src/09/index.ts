8b106474ff4eb4bd9d1adb60d313e9f24e27659a
Fredrik Oterholt <fredrik@technologic.as>
2022-12-09 23:56:48 +0100

    day 09 part 2

--- src/09/index.ts
+++ src/09/index.ts
@@ -14,7 +14,7 @@ const instructions: Instruction[] = input.split(/\n/).map((r) => {
 type Position = { r: number; c: number };
 const initialState: Position = { r: 0, c: 0 };
 
-const moveHead = ({ r, c }: Position, dir: Direction, steps: number): Position => {
+const moveHead = ({ r, c }: Position, dir: Direction, steps = 1): Position => {
   switch (dir) {
     case "R":
       return { r, c: c + 1 };
@@ -29,25 +29,49 @@ const moveHead = ({ r, c }: Position, dir: Direction, steps: number): Position =
   }
 };
 
-const { visits } = instructions.reduce(
-  (state, { dir, steps }) =>
-    numberRange(steps).reduce(({ head: { r, c }, tail: { r: tR, c: tC }, visits: prevVisits }) => {
-      const head = moveHead({ r, c }, dir, steps);
+function moveInDirection(diff: number, newPos: Position, above: Direction, below: Direction) {
+  if (diff > 0) return moveHead(newPos, above);
+  if (diff < 0) return moveHead(newPos, below);
+  return newPos;
+}
 
-      const rDiff = Math.abs(head.r - tR);
-      const cDiff = Math.abs(head.c - tC);
-      const isClose = rDiff > 1 || cDiff > 1;
+const moveTailInCol = (cDiff: number, newPos: Position): Position => moveInDirection(cDiff, newPos, "L", "R");
+const moveTailInRow = (rDiff: number, newPos: Position): Position => moveInDirection(rDiff, newPos, "D", "U");
 
-      const tail = isClose ? { r, c } : { r: tR, c: tC };
+const moveKnot = (head: Position, tail: Position): Position => {
+  const rDiff = tail.r - head.r;
+  const cDiff = tail.c - head.c;
 
-      return { head, tail, visits: [...prevVisits, tail] };
-    }, state),
-  { head: initialState, tail: initialState, visits: [] }
-);
-const uniqueVisits = new Set(visits.map(({ r, c }) => `${r},${c}`));
+  const { r } = moveTailInRow(rDiff, tail);
+  const { c } = moveTailInCol(cDiff, tail);
 
-const part1 = uniqueVisits.size;
-const part2 = "";
+  return { r, c };
+};
+
+const uniqueTailVisits = (knots) =>
+  instructions.reduce(
+    (state, { dir, steps }) =>
+      numberRange(steps).reduce(({ head: { r, c }, tails: prevTails, visits: prevVisits }) => {
+        const head = moveHead({ r, c }, dir, steps);
+
+        const tails = prevTails.map(({ r: tR, c: tC }, i, others) => {
+          const prev = i === 0 ? head : others[i - 1];
+          const rDiff = Math.abs(prev.r - tR);
+          const cDiff = Math.abs(prev.c - tC);
+          const isClose = rDiff > 1 || cDiff > 1;
+          const curr = { r: tR, c: tC };
+          return isClose ? moveKnot(prev, curr) : curr;
+        });
+
+        return { head, tails, visits: [...prevVisits, tails.at(-1)] };
+      }, state),
+    { head: initialState, tails: numberRange(knots).map(() => initialState), visits: [] }
+  );
+
+const getUnique = ({ visits }: { visits: Position[] }) => new Set(visits.map(({ r, c }) => `${r},${c}`));
+
+const part1 = getUnique(uniqueTailVisits(1)).size;
+const part2 = getUnique(uniqueTailVisits(9)).size;
 
 console.group(`2022-day08 ${fileName}`);
 console.log("part 1", part1);
