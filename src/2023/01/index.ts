1e21b4121d36d5b16dfe37ca1e57f239a377679a
Fredrik Oterholt <fredrik@technologic.as>
2023-12-01 23:43:52 +0100

    Add 2023-01/02

--- src/2023/01/index.ts
+++ src/2023/01/index.ts
@@ -2,24 +2,36 @@ import getInputs from "../../helpers/getInputs";
 
 const { input, fileName } = getInputs(__dirname);
 
-const data = input
-  .split(/\n\n/)
-  .map((elfRow) =>
-    elfRow
-      .split(/\n/)
-      .map((n) =>
-        Array.from(n)
-          .map((x) => parseInt(x, 10))
-          .filter((num) => !Number.isNaN(num)),
-      )
-      .map((nums) => [nums.at(0), nums.at(-1)]),
-  )
-  .map((nums) => nums.map((n) => n.join("")))
-  .map((x) => x.map((y) => parseInt(y, 10)).reduce((a, b) => a + b, 0));
+const part2Replacer = (row: string) =>
+  row
+    .replaceAll("one", "one1one")
+    .replaceAll("two", "two2two")
+    .replaceAll("three", "three3three")
+    .replaceAll("four", "four4four")
+    .replaceAll("five", "five5five")
+    .replaceAll("six", "six6six")
+    .replaceAll("seven", "seven7seven")
+    .replaceAll("eight", "eight8eight")
+    .replaceAll("nine", "nine9nine");
 
-const part1 = data;
+const getData = (replacer = (s): string => s) =>
+  input
+    .split(/\n\n/)
+    .map((elfRow) =>
+      elfRow
+        .split(/\n/)
+        .map(replacer)
+        .map((n) =>
+          Array.from(n)
+            .map((x) => parseInt(x, 10))
+            .filter((num) => !Number.isNaN(num)),
+        )
+        .map((nums) => [nums.at(0), nums.at(-1)]),
+    )
+    .map((nums) => nums.map((n) => n.join("")))
+    .map((x) => x.map((y) => parseInt(y, 10)).reduce((a, b) => a + b, 0));
 
 console.group(`2023-day01 ${fileName}`);
-console.log("part 1", part1);
-// console.log("part 2", part2);
+console.log("part 1", getData());
+console.log("part 2", getData(part2Replacer));
 console.groupEnd();
