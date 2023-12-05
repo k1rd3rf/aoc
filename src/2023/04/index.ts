6808291d82f0da9fea71d059909d5cc5d11fc173
Fredrik Oterholt <fredrik@technologic.as>
2023-12-05 20:54:41 +0100

    Add 2023-04/02

--- src/2023/04/index.ts
+++ src/2023/04/index.ts
@@ -9,14 +9,31 @@ const getData = () =>
     .map(([, card, winning, numbers]) => ({ card, winning: winning.split(/\s+/), numbers: numbers.split(/\s+/) }));
 
 console.group(`2023-day04 ${fileName}`);
+const getMatches = () =>
+  getData().map(({ card, winning, numbers }, index) => {
+    const matches = numbers.filter((n) => winning.includes(n));
+    const points = matches.reduce((t) => (t || 0.5) * 2, 0);
+    return { card, winning, numbers, matches, points, wins: matches.length, index };
+  });
+
 console.log(
   "part 1",
-  getData()
-    .map(({ winning, numbers }) => {
-      const wins = numbers.filter((n) => winning.includes(n));
-      const points = wins.reduce((t) => (t || 0.5) * 2, 0);
-      return { wins, points };
-    })
-    .reduce((total, { points }) => total + points, 0),
+  getMatches().reduce((total, { points }) => total + points, 0),
 );
+
+type Card = { wins: number; index: number };
+
+const lookupWinners = (all: Card[], cards = all): Card[] =>
+  cards.concat(
+    cards.flatMap((c) => {
+      if (c.wins) {
+        const next = c.index + 1;
+        return lookupWinners(all, all.slice(next, next + c.wins));
+      }
+      return [];
+    }),
+  );
+
+console.log("part 2", lookupWinners(getMatches()).length);
+
 console.groupEnd();
